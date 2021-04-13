95c3329d54ae75d79046ae93e79b2addc00c3486
mloparj10 <72433638+mloparj10@users.noreply.github.com>
2021-04-13 10:17:40 +0200

    Add files via upload

---
+++ Seleccionador de Fondo de pantalla/FONDO2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Funciones
+Encabezado ()
+{
+    cat "ENCABEZADO.txt"
+}
+SeleccionarFoto ()
+{
+    read -p "Escriba la ruta de la imagen: " RUTA
+    if [ -e $RUTA ]
+    then
+        gsettings set org.gnome.desktop.background picture-uri "file://$RUTA"
+        echo "La imagen se ha establecido"
+    else
+        echo "La imagen no se encuentra"
+        exit 1
+    fi
+}
+#Bloq.Principal
+Encabezado
+SeleccionarFoto
